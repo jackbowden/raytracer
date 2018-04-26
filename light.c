f2d0f647d5eda1872b31f46d899c60405cd7111b
Jack Bowden <jakallan3@gmail.com>
2018-04-25 22:45:54 -0400

    Added light files

---
+++ light.c
@@ -0,0 +1,10 @@
+#include "math.h" //DO I NEED THIS
+#include "light.h"
+
+//FILL IN STUFF
+
+//computes ambient, diffuse, and specular lighting; returns final color
+COLOR_T illuminate (RAY_T ray, LIGHT_T light, OBJ_T *list, COLOR_T surface_col, VEC_T int_pt, VEC_T normal);
+//returns true if int_point is in shadow
+static int shadow_test (LIGHT_T light, OBJ_T *list, VEC_T int_pt);
+
---
+++ light.h
@@ -0,0 +1,13 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "vec.h"
+#include "rt.h" //is this needed?
+
+typedef struct LIGHT_T {
+    VEC_T loc;
+} LIGHT_T;
+
+//function prototype?
+
+#endif
